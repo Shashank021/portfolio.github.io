f6f2e9175d06a4f6a41334d6a2e8a98c352f933f
Shashank <89263450+Shashank021@users.noreply.github.com>
2022-10-02 19:42:06 +0530

    Add files via upload

--- js/service.js
+++ js/service.js
@@ -4,6 +4,7 @@ const service = () => {
     service.innerHTML =`
         <div class="section3" id="section3">
             <h1>Service</h1>
+            <div class="box">h</div>
         </div><br><br><br><br><br><br><br><br><br><br><br>
     `;
 }
