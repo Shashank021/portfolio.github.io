546fd6b84a741740ecbe4ed46402ea10b8921886
Shashank <89263450+Shashank021@users.noreply.github.com>
2022-10-03 18:01:53 +0530

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,16 @@ const footer = () => {
     
     footer.innerHTML =`
         <div class="footer">
-            <h1>Footer</h1>
+            <div class="bottom">
+                <ul>
+                    <li>about</li>
+                    <li>project</li>
+                    <li>achievements</li>
+                    <li>service</li>
+                    <li>contact</li>
+                </ul>
+            </div>
+            <div class="last">© 2022 Shashank Vats. All rights reserved.</div>
         </div><br><br><br><br><br><br><br><br><br><br><br>
     `;
 }
