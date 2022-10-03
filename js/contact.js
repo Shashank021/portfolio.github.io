313fddc0b5cf4170cdecc442d5a0322d898255b5
Shashank <89263450+Shashank021@users.noreply.github.com>
2022-10-03 14:08:21 +0530

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -4,7 +4,20 @@ const contact = () => {
     contact.innerHTML =`
         <div class="section4" id="section4">
             <h1>Contact</h1>
-        </div><br><br><br><br><br><br><br><br><br><br><br>
+            <div class="contact">
+                <div>
+                    <img src="image/name-logo1.png" class="name-logo" alt=""><br><br><br><br><br>
+                    <a>+91-9027659714</a><br>
+                    <a>shashankvats2112@gmail.com</a>
+                </div>
+                <form>
+                    <input type="textbox" placeholder="your name"><br>
+                    <input type="textbox" placeholder="your email"><br>
+                    <input type="textbox" placeholder="your message"><br>
+                    <a><button>submit</button><a>
+                </form>
+            </div>
+        </div><br><br><br><br><br><br><br>
     `;
 }
 contact();
--- js/service.js
+++ js/service.js
@@ -4,7 +4,11 @@ const service = () => {
     service.innerHTML =`
         <div class="section3" id="section3">
             <h1>Service</h1>
-            <div class="box">h</div>
+            <div class="box">
+                <div class="box0"><\\><br><br><b>Web Design</b><br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit quidem aliquid tempore culpa nam autem facere, repellat repudiandae impedit debitis possimus sequi cupiditate nihil ab! Aliquid officia odio autem!</div>
+                <div class="box1">[]<br><br><b>UI/UX Design</b><br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit quidem aliquid tempore culpa nam autem facere, repellat repudiandae impedit debitis possimus sequi cupiditate nihil ab! Aliquid officia odio autem!</div>
+                <div class="box2">$<br><br><b>App Design</b><br><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate odit quidem aliquid tempore culpa nam autem facere, repellat repudiandae impedit debitis possimus sequi cupiditate nihil ab! Aliquid officia odio autem!</div>
+            </div>
         </div><br><br><br><br><br><br><br><br><br><br><br>
     `;
 }
